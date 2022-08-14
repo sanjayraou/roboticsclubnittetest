d0a45d65ddc5e14d05983ca1bdf18903a1784eab
U Sanjay Rao <65221307+sanjayraou@users.noreply.github.com>
2022-08-14 21:35:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onscroll = function() { scrollFunction() };
+
 var check = document.getElementById("check");
 var icon=document.getElementById("menu-img");
 var nav_list=document.getElementById("menu")
@@ -18,9 +18,11 @@ function hide(){
  icon.src="Images/Menu Icon.png";
     
 }
-function scrollFunction() {
+window.onscroll = function() { scrollFunction() };
+  function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("nav").style.backgroundColor = "#fff"
+        document.getElementById("nav").style.top = "0"
         document.getElementById("logo-p").style.color="#194d82"
         document.getElementById("nav").style.borderBottom = "2px solid #194d82";
         document.getElementById("logo-img").src="Images/Logo_color.png"
@@ -41,6 +43,7 @@ function scrollFunction() {
         document.getElementById("nav").style.borderBottom = "none";
         document.getElementById("nav-btn").style.color = "#fff"
         var elms = document.querySelectorAll("[id='nav-btn']");
+        document.getElementById("nav").style.top = "auto"
 
         for (var i = 0; i < elms.length; i++)
             elms[i].style.color = '#fff';
@@ -48,7 +51,25 @@ function scrollFunction() {
       for (var i = 0; i < element.length; i++)
             element[i].style.setProperty("--check-primary", "#fff");
     }
+var mq = window.matchMedia( "(max-width: 768px)" );
+if (mq.matches) {
+   document.getElementById("nav").style.backgroundColor = "transparent"
+        document.getElementById("logo-p").style.color="#fff"
+        document.getElementById("logo-img").src="Images/White Logo.png"
+        document.getElementById("nav").style.borderBottom = "none";
+        document.getElementById("nav-btn").style.color = "#fff"
+        var elms = document.querySelectorAll("[id='nav-btn']");
+
+        for (var i = 0; i < elms.length; i++)
+            elms[i].style.color = '#fff';
+                const element = document.querySelectorAll(".li-btn");
+      for (var i = 0; i < element.length; i++)
+            element[i].style.setProperty("--check-primary", "#fff");
+             document.getElementById("nav").style.position = "absolute"
+
 }
-$(window).on("load",function(){
-     $(".loader-wrapper").fadeOut("slow");
-});
+else {
+  
+}
+    // window width is greater than 570px
+}
